68ecbddc37509d1501f3b8c8ad9c2c5bba1cd09e
thelmasv <thelmasv@gmail.com>
2019-02-06 09:33:14 +0100

    småändring

--- js/controller/dishDetailsController.js
+++ js/controller/dishDetailsController.js
@@ -5,13 +5,13 @@ var DishDetailsController = function(view, model, app) {
 	view.goBack.click(function() { //skapa ett namn på vad som ska klickas på och koppla den till index
 		app.showDishSelectScreen(); // VÄLJ VART DEN SKA GÅ
 		console.log("go back to previous page");
-
+})
 		console.log(view); 
 	view.addToMenu.click(function() { //skapa ett namn på vad som ska klickas på och koppla den till index
 		app.showSelectAgainScreen(); // gå till selectagain
 		console.log("add to sidebar");   
 
-	})
+	
 })
 
 }
